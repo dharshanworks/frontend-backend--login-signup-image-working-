8758f0f46d53a6badfaeb41e05d083c3044d6e3c
Dharshan R <dharshanr23@gmail.com>
2025-07-21 20:23:12 +0530

    Update Footer.jsx

--- client/src/components/Footer.jsx
+++ client/src/components/Footer.jsx
@@ -3,19 +3,26 @@ import { assets } from '../assets/assets'
 
 const Footer = () => {
   return (
-    <div className='flex items-center justify-between gap-4 py-3 mt-20'>
+    <div className='flex flex-col sm:flex-row items-center justify-between gap-4 py-3 mt-20'>
 
-        <img src={assets.logo} alt="" width={150}  />
-        <p className='flex-1 border-l  border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>
-            Copyright @Dharshan | All right reserved.
-        </p>
+      <img src={assets.logo} alt="Company Logo" width={150} />
 
-        <div className='flex gap-2.5'>
-            <img src={assets.facebook_icon} alt="" width={35} />
-            <img src={assets.instagram_icon} alt="" width={35} />
-            <img src={assets.twitter_icon} alt="" width={35} />
-        </div>
-        
+      <p className='flex-1 border-l border-gray-400 pl-4 text-sm text-gray-500 max-sm:hidden'>
+        © Dharshan | All rights reserved.
+      </p>
+
+      <div className='flex gap-2.5'>
+        <a href="https://facebook.com" target="_blank" rel="noopener noreferrer">
+          <img src={assets.facebook_icon} alt="Facebook" width={35} />
+        </a>
+        <a href="https://instagram.com" target="_blank" rel="noopener noreferrer">
+          <img src={assets.instagram_icon} alt="Instagram" width={35} />
+        </a>
+        <a href="https://twitter.com" target="_blank" rel="noopener noreferrer">
+          <img src={assets.twitter_icon} alt="Twitter" width={35} />
+        </a>
+      </div>
+
     </div>
   )
 }
